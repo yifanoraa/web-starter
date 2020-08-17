f4557088b0d975a994e9ccb87e85ac4351e6efe5
YifanLiu <aaron.liu@ubtrobot.com>
2020-08-17 14:35:43 +0800

    Initial commit

---
+++ spring-boot-sample/src/main/java/com/yifanoraa/core/dao/TestDao.java
@@ -0,0 +1,9 @@
+package com.yifanoraa.core.dao;
+
+public interface TestDao {
+
+    int InsertContent();
+
+    void FetchContent();
+
+}
---
+++ spring-boot-sample/src/test/java/com/yifanoraa/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yifanoraa.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
